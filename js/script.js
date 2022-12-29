3d0dd963dd0ce7b6202812ff6c01f2e8ec186644
ceciaups <cecilia127@msn.com>
2022-12-29 10:32:53 -0500

    Add js to active panel

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ window.onload = function() {
   // Variables
   const navBar = document.getElementById("nav-bar");
   const navToggle = document.getElementById("nav-toggle");
+  var play;
   var mobile = window.matchMedia("(max-width: 425px)");
 
   // Event Listener
   navToggle.addEventListener("click", toggleMenu, false);
-  slideshow(mobile);
+  window.addEventListener("scroll", reveal);
+  reveal();
   mobile.addEventListener("change", slideshow);
 
   // Toggle Function
@@ -18,12 +20,22 @@ window.onload = function() {
 
   function slideshow(mobile) {
     if (mobile.matches) {
-      var slider = document.getElementById("slider-ul");
-      var play = playSlides(5000);
-      slider.addEventListener("click", function() {
-        clearInterval(play);
-        play = playSlides(10000);
-      })
+      var secProjects = document.getElementById("sec-projects");
+      if (secProjects.classList.contains("active")) {
+        
+        var slider = document.getElementById("slider-ul");
+        play = playSlides(5000);
+        slider.addEventListener("click", function() {
+          clearInterval(play);
+          setTimeout(function() {
+            play = playSlides(5000);
+          }, 5000);
+          
+        })
+      }
+    }
+    else {
+      clearInterval(play);
     }
   }
   
@@ -41,5 +53,28 @@ window.onload = function() {
         }
       }
     }, time);
+  }
+
+  function reveal() {
+    var reveals = document.getElementsByClassName("reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementBottom = reveals[i].getBoundingClientRect().bottom;
+      var windowHeight = window.innerHeight;
+
+      if (elementTop < windowHeight && elementBottom > 0) {
+        if (!reveals[i].classList.contains("active")) {
+          reveals[i].classList.add("active");
+          if (reveals[i].id == "sec-projects")
+            slideshow(mobile);
+        }
+      }
+      else {
+        reveals[i].classList.remove("active");
+        if (reveals[i].id == "sec-projects")
+          clearInterval(play);
+      }
+    }
   }
 }
